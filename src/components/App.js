7e4d99e17ebe07a0760759d715db675860a87246
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-10-12 16:30:42 +0200

    Copy React Boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="flexbox-column fullscreen">
+            <p>Tic-Tac-Toe</p>
+        </div>
+    )
+}
+
+export { App as default }
