422eb745490f47797fca650e1cbc8e02ce0a21a3
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-10-13 21:09:23 +0200

    Add Footer.js and _footer.scss files

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>Created with &#x1F49B; by Łukasz Szymański</p>
+        </footer>
+    )
+}
+
+export { Footer as default }
