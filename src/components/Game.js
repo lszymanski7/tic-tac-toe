d9995015dc3097d841e14c23d98d010dacd31c68
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-10-14 01:54:36 +0200

    Add Scoreboard.js file

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
+import Scoreboard from './Scoreboard'
 
 const Game = () => {
     return (
         <div className="flexbox-column fullscreen">
             <Header />
+            <Scoreboard />
             <Footer />
         </div>
     )
---
+++ src/components/Scoreboard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Scoreboard = () => {
+    return (
+        <div className="flexbox-row">
+            <div className="flexbox-column">
+                <span>Player 1 (X)</span>
+                <span>0</span>
+            </div>
+            <div className="flexbox-column">
+                <span>Draw</span>
+                <span>1</span>
+            </div>
+            <div className="flexbox-column">
+                <span>Player 2 (O)</span>
+                <span>2</span>
+            </div>
+        </div>
+    )
+}
+
+export { Scoreboard as default }
