896259c08222180b3ed547b6f956b690baf63107
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-10-14 01:09:21 +0200

    Add Game.js file and update index.js file

---
+++ src/components/Game.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+
+const Game = () => {
+    return (
+        <div className="flexbox-column fullscreen">
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export { Game as default }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import Game from './components/Game'
 import '@csstools/normalize.css'
 import './assets/sass/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<App />)
+root.render(<Game />)
