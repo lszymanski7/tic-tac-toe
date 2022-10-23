78cfd764a72f50543ee3eed2cbf6e75271b980ca
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-10-23 22:58:52 +0200

    Update Cell.js, Game.js and game-rules.js files

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import xIcon from '../assets/images/shapes/x.svg'
+import oIcon from '../assets/images/shapes/o.svg'
 
 const Cell = () => {
+    const [icon, setIcon] = useState(xIcon)
+
+    const test = () => {
+        setIcon(oIcon)
+        console.log('test')
+    }
+
     return (
         <div>
-            <button>Square</button>
+            <img src={icon} alt="test" onClick={test} />
         </div>
     )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Board from './Board'
 import Footer from './Footer'
 import Header from './Header'
 import Score from './Score'
+import checkWinner from '../utils/game-rules'
 
 const Game = () => {
+    const [board, setBoard] = useState(['X', 'O', '', 'X', 'O', '', 'X', '', ''])
+    const winner = checkWinner(board)
+
+    console.log(board)
+    console.log(winner)
+
     return (
         <div className="flexbox-column fullscreen">
             <Header />
             <Score />
-            <Board />
+            <Board board={board} setBoard={setBoard} />
             <Footer />
         </div>
     )
