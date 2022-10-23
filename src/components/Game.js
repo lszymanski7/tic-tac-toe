b500fec8efb4529c9bd0c368a7caf3706c38c32d
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-10-23 23:17:55 +0200

    Update Game.js and Score.js files

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,15 +7,22 @@ import checkWinner from '../utils/game-rules'
 
 const Game = () => {
     const [board, setBoard] = useState(['X', 'O', '', 'X', 'O', '', 'X', '', ''])
+    const [score, setScore] = useState({
+        draw: 0,
+        player1: 0,
+        player2: 0
+    })
+    
     const winner = checkWinner(board)
 
     console.log(board)
+    console.log(score)
     console.log(winner)
 
     return (
         <div className="flexbox-column fullscreen">
             <Header />
-            <Score />
+            <Score score={score} />
             <Board board={board} setBoard={setBoard} />
             <Footer />
         </div>
--- src/components/Score.js
+++ src/components/Score.js
@@ -1,22 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Score = () => {
+const Score = ({ score }) => {
     return (
         <div className="flexbox-row">
             <div className="flexbox-column">
                 <span>Player 1 (X)</span>
-                <span>0</span>
+                <span>{score.player1}</span>
             </div>
             <div className="flexbox-column">
-                <span>Tie</span>
-                <span>1</span>
+                <span>Draw</span>
+                <span>{score.draw}</span>
             </div>
             <div className="flexbox-column">
                 <span>Player 2 (O)</span>
-                <span>2</span>
+                <span>{score.player2}</span>
             </div>
         </div>
     )
+}
+
+Score.propTypes = {
+    score: PropTypes.object.isRequired
 }
 
 export { Score as default }
