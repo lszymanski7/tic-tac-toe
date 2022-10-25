f070e6344e6a89b753525beed7f6c80c2b5fd94d
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-10-26 01:57:55 +0200

    Update Board.js file

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,42 +7,51 @@ const Board = ({ board }) => {
         <div className="flexbox-row">
             <div className="flexbox-column">
                 <Cell
+                    className={'cell'}
                     index={0}
                     symbol={board[0]}
                 />
                 <Cell
+                    className={'cell top bottom'}
                     index={3}
                     symbol={board[3]}
                 />
                 <Cell
+                    className={'cell'}
                     index={6}
                     symbol={board[6]}
                 />
             </div>
             <div className="flexbox-column">
                 <Cell
+                    className={'cell left right'}
                     index={1}
                     symbol={board[1]}
                 />
                 <Cell
+                    className={'cell left right top bottom'}
                     index={4}
                     symbol={board[4]}
                 />
                 <Cell
+                    className={'cell left right'}
                     index={7}
                     symbol={board[7]}
                 />
             </div>
             <div className="flexbox-column">
                 <Cell
+                    className={'cell'}
                     index={2}
                     symbol={board[2]}
                 />
                 <Cell
+                    className={'cell top bottom'}
                     index={5}
                     symbol={board[5]}
                 />
                 <Cell
+                    className={'cell'}
                     index={8}
                     symbol={board[8]}
                 />
