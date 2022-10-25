cca5b0c93377b368990e47d57c728cadeccdcd8e
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-10-25 21:56:22 +0200

    Update Board.js file

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,26 +1,58 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Cell from './Cell'
 
-const Board = () => {
+const Board = ({ board }) => {
     return (
         <div className="flexbox-row">
             <div className="flexbox-column">
-                <Cell />
-                <Cell />
-                <Cell />
+                <Cell
+                    index={0}
+                    symbol={board[0]}
+                />
+                <Cell
+                    index={3}
+                    symbol={board[3]}
+                />
+                <Cell
+                    index={6}
+                    symbol={board[6]}
+                />
             </div>
             <div className="flexbox-column">
-                <Cell />
-                <Cell />
-                <Cell />
+                <Cell
+                    index={1}
+                    symbol={board[1]}
+                />
+                <Cell
+                    index={4}
+                    symbol={board[4]}
+                />
+                <Cell
+                    index={7}
+                    symbol={board[7]}
+                />
             </div>
             <div className="flexbox-column">
-                <Cell />
-                <Cell />
-                <Cell />
+                <Cell
+                    index={2}
+                    symbol={board[2]}
+                />
+                <Cell
+                    index={5}
+                    symbol={board[5]}
+                />
+                <Cell
+                    index={8}
+                    symbol={board[8]}
+                />
             </div>
         </div>
     )
+}
+
+Board.propTypes = {
+    board: PropTypes.array.isRequired
 }
 
 export { Board as default }
