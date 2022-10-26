9c58ecf345c1570d44c9f4513d7a5e66e56cdb11
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-10-26 02:40:22 +0200

    Update Cell.js file

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,20 +1,32 @@
-import React, { useState } from 'react'
-import xIcon from '../assets/images/shapes/x.svg'
+import React from 'react'
+import PropTypes from 'prop-types'
 import oIcon from '../assets/images/shapes/o.svg'
+import xIcon from '../assets/images/shapes/x.svg'
 
-const Cell = () => {
-    const [icon, setIcon] = useState(xIcon)
-
-    const test = () => {
-        setIcon(oIcon)
-        console.log('test')
-    }
+const Cell = ({ className, index, symbol }) => {
+    const icon = symbol === 'O' ? oIcon : symbol === 'X' ? xIcon : symbol
 
     return (
-        <div>
-            <img src={icon} alt="test" onClick={test} />
+        <div className={className}>
+            <button
+                className="cell__button"
+                onClick={() => console.log('Clicked:', index)}
+            >
+                {icon && (
+                    <img
+                        alt={`${symbol} Icon`}
+                        src={icon}
+                    />
+                )}
+            </button>
         </div>
     )
+}
+
+Cell.propTypes = {
+    className: PropTypes.string.isRequired,
+    index: PropTypes.number.isRequired,
+    symbol: PropTypes.string
 }
 
 export { Cell as default }
