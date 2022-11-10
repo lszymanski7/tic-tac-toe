2b46e180fa27a90a20b90ceadb2223fb2fac2a51
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-11-10 21:49:31 +0100

    Update Header.js, _flexbox.scss and _header.scss files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ToggleSwitch from './ToggleSwitch'
+import github from '../assets/images/shapes/github.svg'
 
 const Header = () => {
     const title = 'Tic-Tac-Toe'
+    const [isChecked, setIsChecked] = useState(false)
 
     return (
-        <header>
-            <div className="flexbox-column">
-                <h1>{title}</h1>
+        <header className="flexbox-column header">
+            <div className="flexbox-row header__container">
+                <a href="https://github.com/lszymanski7/tic-tac-toe">
+                    <img
+                        alt="GitHub"
+                        className="header__github"
+                        src={github}
+                    />
+                </a>
+                <ToggleSwitch
+                    handleOnChange={() => setIsChecked(!isChecked)}
+                    id="themeSwitcher"
+                    isChecked={isChecked}
+                />
             </div>
+            <h1 className="header__title">{title}</h1>
         </header>
     )
 }
