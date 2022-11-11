d6edfb172c544d45414298ab8ec930240f52f76f
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-11-11 02:34:24 +0100

    Add _score.scss file and update Score.js file

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import oBracketsIcon from '../assets/images/shapes/o-brackets.svg'
+import xBracketsIcon from '../assets/images/shapes/x-brackets.svg'
 
 const Score = ({ score }) => {
     return (
-        <div className="flexbox-row">
-            <div className="flexbox-column">
-                <span>Player 1 (X)</span>
-                <span>{score.player1}</span>
+        <div className="flexbox-row score">
+            <div className="flexbox-column score__container">
+                <div className="flexbox-row score__player">
+                    <p>Player 1</p>
+                    <img
+                        className="score__img"
+                        src={xBracketsIcon}
+                    />
+                </div>
+                <p>{score.player1}</p>
             </div>
-            <div className="flexbox-column">
-                <span>Draw</span>
-                <span>{score.draw}</span>
+            <div className="flexbox-column score__container">
+                <p>Draw</p>
+                <p>{score.draw}</p>
             </div>
-            <div className="flexbox-column">
-                <span>Player 2 (O)</span>
-                <span>{score.player2}</span>
+            <div className="flexbox-column score__container">
+                <div className="flexbox-row score__player">
+                    <p>Player 2</p>
+                    <img
+                        className="score__img"
+                        src={oBracketsIcon}
+                    />
+                </div>
+                <p>{score.player2}</p>
             </div>
         </div>
     )
