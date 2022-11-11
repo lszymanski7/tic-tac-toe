261d316b9ac28ed27007689f1a6608c642ac3f72
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2022-11-11 19:27:37 +0100

    Update Square.js, main.scss, _board.scss and _square.scss files

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types'
 import oIcon from '../assets/images/shapes/o.svg'
 import xIcon from '../assets/images/shapes/x.svg'
 
-const Cell = ({ className, index, onClick, symbol }) => {
+const Square = ({ className, handleOnClick, symbol }) => {
     const icon = symbol === 'O' ? oIcon : symbol === 'X' ? xIcon : symbol
 
     return (
         <div className={className}>
             <button
-                className="cell__button"
-                onClick={() => onClick(index)}
+                className="square__button"
+                onClick={handleOnClick}
             >
                 {icon && (
                     <img
                         alt={`${symbol} Icon`}
+                        className="square__symbol"
                         src={icon}
                     />
                 )}
@@ -23,11 +24,10 @@ const Cell = ({ className, index, onClick, symbol }) => {
     )
 }
 
-Cell.propTypes = {
+Square.propTypes = {
     className: PropTypes.string.isRequired,
-    index: PropTypes.number.isRequired,
-    onClick: PropTypes.func.isRequired,
+    handleOnClick: PropTypes.func.isRequired,
     symbol: PropTypes.string
 }
 
-export { Cell as default }
+export { Square as default }
