a0b5becc2ad59e6f631844f3e04d2cafd4a803bd
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2023-02-22 23:56:10 +0100

    Update index.js file - wrap Game component with ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+import '@csstools/normalize.css'
+import './assets/sass/main.scss'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Game from './components/Game'
-import '@csstools/normalize.css'
-import './assets/sass/main.scss'
+import { ThemeProvider } from './contexts/theme-context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Game />)
+root.render(
+    <ThemeProvider>
+        <Game />
+    </ThemeProvider>
+)
