97eb545651c43da784e5539cfe9b6b0f47b793e7
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2023-02-24 16:43:18 +0100

    Add constants.js file and update images format

---
+++ src/data/constants.js
@@ -0,0 +1,10 @@
+const LOGOS = {
+    tic_tac_toe: {
+        size_128x128: require('../assets/images/logos/tic-tac-toe-128x128.webp'),
+        size_256x256: require('../assets/images/logos/tic-tac-toe-256x256.webp'),
+        size_512x512: require('../assets/images/logos/tic-tac-toe-512x512.webp')
+    },
+    github: require('../assets/images/logos/github.svg')
+}
+
+export { LOGOS }
